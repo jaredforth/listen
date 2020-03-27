828abb42e0be7e7b305e264c8264a370cd8bef8b
Jared Forth <jaredforthdev@gmail.com>
2020-03-27 16:32:56 -0500

    start to work

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,38 @@
+pub enum Event {
+    OnFileAdd,
+    OnFileRemove
+}
+
+pub struct ListenerOptions<T, R> {
+    /// The path to listen on
+    path: &'static str,
+    /// The event to listen for
+    event: Event,
+    /// The argument to pass to `func`
+    arg: T,
+    /// Function to execute when event happens
+    func: fn(T) -> R
+}
+
+/// This function takes a path to listen on, the event to listen for,
+/// and a function to execute when that event happens.
+pub fn listen<T, R>(path: &str, event: Event, arg: T, func: fn(T) -> R) {
+    func(arg);
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::{listen, Event};
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_listen() {
+        // Create test directory
+        fsutils::mkdir("test_directory");
+        fn print_to_console(arg: &str) {
+            println!("file added {}", arg);
+        }
+        let l = listen::<&str, ()>("test_directory", Event::OnFileAdd, "the function argument", print_to_console);
+        // Clean up
+        fsutils::rmdir("test_directory");
     }
 }
