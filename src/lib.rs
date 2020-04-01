0e7d3d5e5b1abbefc55073acf72f3e508cced335
Jared Forth <jaredforthdev@gmail.com>
2020-04-01 10:22:21 -0500

    restructure a bit

--- src/lib.rs
+++ src/lib.rs
@@ -16,69 +16,75 @@ pub enum Event {
 
 /// `ListenerOptions` contains the configuration details for
 /// The `listen` function.
-pub struct ListenerOptions<T, R> {
+pub struct Listener {
     /// The path to listen on
-    path: &'static str,
+    // path: &'static str,
     /// The event to listen for
     event: Event,
-    /// The argument to pass to `func`
-    arg: T,
-    /// Function to execute when event happens
-    func: fn(T) -> R,
+    // The argument to pass to `func`
+    // arg: T,
+    // Function to execute when event happens
+    // func: fn(T) -> R,
 }
 
-/// This function takes a path to listen on, the event to listen for,
-/// and a function to execute when that event happens.
-pub fn listen<T, R>(path: &str, event: Event, arg: T, func: fn(T) -> R) {
-    match event {
-        Event::OnFileAdd => {
-            if listener(Event::OnFileAdd, path) {
-                func(arg);
-            }
-        }
-        Event::OnFileRemove => {
-            if listener(Event::OnFileRemove, path) {
-                func(arg);
-            }
+impl Listener {
+    pub fn new() -> Listener {
+        Listener {
+            event: Event::OnFileChange
         }
     }
-}
-
-fn listener(_event: Event, path: &str) -> bool {
-    let mut changed: bool = false;
-    let initial_count = count_directory_files(path);
-
-    while !changed {
-        let count = count_directory_files(path);
-        if count == initial_count {
-            continue;
-        } else {
-            changed = true
-        }
-    }
-
-    changed
-}
-
-fn count_directory_files(path: &str) -> i64 {
-    let mut count = 0;
-    for entry in WalkDir::new(path).max_depth(1) {
-        match entry {
-            Ok(entry) => {
-                let path = entry.path();
-                // Check if is file
-                if !path.is_dir() {
-                    // println!("Path: {:?}", path);
-                    count = count + 1;
-                    //     // Check if is CSV file and push to vector
-                    //     if path.extension() == Some(OsStr::new("csv")) {
-                    //         let path_str = String::from(path.to_str().unwrap());
-                    //         studies.push(path_str);
-                    //     }
+    /// This function takes a path to listen on, the event to listen for,
+    /// and a function to execute when that event happens.
+    pub fn listen<T, R>(&self, path: &str, arg: T, func: fn(T) -> R) {
+        match self.event {
+            Event::OnFileChange => {
+                if self.file_change_listener(path) {
+                    func(arg);
                 }
+            },
+            Event::OnFileAdd => {
+                unimplemented!()
             }
-            Err(_) => ()
+            Event::OnFileRemove => {
+                unimplemented!()
+            }
         }
     }
-    count
+    fn file_change_listener(&self, path: &str) -> bool {
+        let mut changed: bool = false;
+        let initial_count = self.count_directory_files(path);
+
+        while !changed {
+            let count = self.count_directory_files(path);
+            if count == initial_count {
+                continue;
+            } else {
+                changed = true
+            }
+        }
+
+        changed
+    }
+    fn count_directory_files(&self, path: &str) -> i64 {
+        let mut count = 0;
+        for entry in WalkDir::new(path).max_depth(1) {
+            match entry {
+                Ok(entry) => {
+                    let path = entry.path();
+                    // Check if is file
+                    if !path.is_dir() {
+                        // println!("Path: {:?}", path);
+                        count = count + 1;
+                        //     // Check if is CSV file and push to vector
+                        //     if path.extension() == Some(OsStr::new("csv")) {
+                        //         let path_str = String::from(path.to_str().unwrap());
+                        //         studies.push(path_str);
+                        //     }
+                    }
+                }
+                Err(_) => ()
+            }
+        }
+        count
+    }
 }
